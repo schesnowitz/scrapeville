9f5e4f8fa243039f9fc8f6e62bbf8ca293075b25
steve chesnowitz <steve@chesnowitz.com>
2016-04-22 18:47:54 +0000

    can

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem 'nokogiri', '~> 1.6', '>= 1.6.7.2'
 gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootstrap-rails.git'
 gem 'simple_form', '~> 3.2', '>= 3.2.1'
 gem 'devise', '~> 3.5', '>= 3.5.6'
-
+gem 'cancancan', '~> 1.13', '>= 1.13.1'
 
 
 group :development, :test do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  # def google_news
-  #   require 'open-uri'
-  #   page = Nokogiri::HTML(open('https://news.google.com/news/'))
-  #   render text: page
-  # end
+    rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path
+    flash[:alert] = "Sorry, you are not authorized to access this area!"
+  end
 end
